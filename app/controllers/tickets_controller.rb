9c9703e5799c1d4be7aaf4f3d0838f015a67259f
Eu-Loong Lim <euloong@hotmail.com>
2019-06-09 16:45:18 +1000

    error handling

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,9 @@
 class TicketsController < ApplicationController
   require 'will_paginate/array'
 
+  # rescue_from with: :api_unavailable
+  # rescue_from StandardError, with: :internal_server_error
+
   def index
     @tickets = tickets_results['tickets']
     @tickets = @tickets.paginate(page: params[:page], per_page: 25)
@@ -11,4 +14,17 @@ class TicketsController < ApplicationController
   def tickets_results
     Ticket.retrieve_tickets
   end
+
+  # def api_unavailable
+  #   render :api_unavailable
+  # end
+
+  # def internal_server_error(exception)
+  #   if Rails.env.development?
+  #     response = { type: exception.class.to_s, message: exception.message, backtrace: exception.backtrace }
+  #   else
+  #     response = { error: "Internal Server Error" }
+  #   end
+  #   render json: response, status: :internal_server_error
+  # end
 end
